c928a635a0df9405463b4c6c11806c10e3f1b6c8
axelgafu <axel.gafu@gmail.com>
2015-04-11 10:13:59 -0500

    20150411: Removed X label.

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@ data <- read.table( "household_power_consumption.txt", sep=";", header=TRUE, ski
 time <- strptime( paste(data[,1], " ", data[,2]), format="%d/%m/%Y %H:%M:%S" )
 
 png( file="plot3.png" )
-plot( time, data[,7], ylab="Energy sub metering", type="l" )
+plot( time, data[,7], ylab="Energy sub metering", xlab="", type="l" )
 lines( time, data[,8], col="red")
 lines( time, data[,9], col="blue")
 legend( "topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1 )
